5f2ae7eea848fb0f268c235afb7de6bfc204d4de
João Miranda <joaoaimiranda@tutanota.com>
2025-05-11 11:41:15 +0100

    About Page v1
    Added a simple layout with basic YEASTRACT+ information to About.js

--- yet-another-frontend/src/components/About.js
+++ yet-another-frontend/src/components/About.js
@@ -1,5 +1,236 @@
 import React from "react";
 
 export default function About() {
-    return <p>About page</p>;
+  return (
+    <div className="container mx-auto p-6">
+      {/* Welcome Section */}
+      <section className="mb-8">
+        <h2 className="text-2xl font-bold mb-4">Welcome to YEASTRACT+</h2>
+        <p className="text-lg">
+          The <b>YEASTRACT+</b> (<b>Y</b>east <b>S</b>earch for <b>T</b>
+          ranscriptional <b>R</b>egulators <b>A</b>nd <b>C</b>onsensus <b>T</b>
+          racking)<b>+</b> portal is a wide-scope tool for the analysis and
+          prediction of transcription regulatory associations at the gene and
+          genomic levels in yeasts of biotechnological or human health
+          relevance. It provides bioinformatics tools for the prediction and
+          visualization of gene and genomic regulation based on orthologous
+          regulatory associations described for other yeast species, based on
+          comparative genomics.
+        </p>
+      </section>
+
+      {/* Acknowledgments Section */}
+      <section className="mb-8">
+        <h2 className="text-2xl font-bold mb-4">Acknowledgments</h2>
+        <p className="text-lg">
+          This work was partially supported by the Portuguese{" "}
+          <i>Fundação para a Ciência e Tecnologia</i> through POSC and PDTC,
+          namely via:
+        </p>
+
+        <img
+          className="size-auto shrink-0 py-4"
+          src="/img/logo_posc.jpg"
+          alt="logo POSC"
+        />
+
+        <ul className="list-disc list-inside mb-4 text-lg">
+          <li>
+            Project grant POSC/EIA/57398/2004: DBYeast:{" "}
+            <i>
+              Infrastructures and algorithms for analysis and identification of
+              gene regulatory networks
+            </i>
+          </li>
+          <li>
+            Project grant PDTC/BIO/56838/2004:{" "}
+            <i>
+              Insights into the complex regulatory networks acting in Yeast
+              cells challenged with drugs/chemical stresses: genome-wide
+              expression approaches supported by bioinformatics
+            </i>
+          </li>
+          <li>
+            National funds through Programa Operacional Regional de Lisboa 2020,
+            LISBOA-01-0145-FEDER-022231 – the Biodata.pt Research
+            Infrastructure.
+          </li>
+        </ul>
+
+        <p className="text-lg">
+          The information about Yeast genes other than documented regulations,
+          potential regulations and the TF binding sites contained in YEASTRACT
+          are gathered from; Saccharomyces Genome Database (SGD), Gene Ontology
+          (GO) Consortium and Regulatory Sequence Analysis (RSA) Tools.
+        </p>
+      </section>
+
+      {/* Contact Section */}
+      <section className="mb-8">
+        <h2 className="text-2xl font-bold mb-4">Contact</h2>
+        <p className="text-lg">
+          Please send your comments, questions or suggestions to{" "}
+          <a
+            href="mailto:contact@yeastract.com"
+            className="text-blue-500 underline"
+          >
+            contact@yeastract.com
+          </a>
+          .
+        </p>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-bold mb-4">Team</h2>
+        <div className="mb-4">
+          <a
+            href="https://www.inesc-id.pt/research-areas/automated-reasoning-and-software-reliability/"
+            className="text-blue-500 underline"
+          >
+            <h3 className="text-xl font-semibold">
+              Reasoning and Software Reliability Research Group
+            </h3>
+          </a>
+          <ul className="list-disc list-inside">
+            <li>
+              <strong>Pedro T. Monteiro, PhD</strong>, Associate Professor IST,{" "}
+              <a
+                href="http://pedromonteiro.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 underline"
+              >
+                homepage
+              </a>
+            </li>
+            <li>
+              <strong>Jorge Oliveira, PhD</strong>,{" "}
+              <a
+                href="https://web.ist.utl.pt/jorge.oliveira/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 underline"
+              >
+                homepage
+              </a>
+            </li>
+            <li>
+              <strong>João Miranda, PhD student</strong>,{" "}
+              <a
+                href="https://www.cienciavitae.pt/B915-D668-DF0C"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 underline"
+              >
+                homepage
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="mb-4">
+          <a
+            href="http://groups.ist.utl.pt/bsrg/"
+            className="text-blue-500 underline"
+          >
+            <h3 className="text-xl font-semibold">
+              Biological Sciences Research Group
+            </h3>
+          </a>
+          <ul className="list-disc list-inside">
+            <li>
+              <strong>Miguel C. Teixeira, PhD</strong>, Full Professor IST,{" "}
+              <a
+                href="https://sites.google.com/view/ibb-tecnico-bsrg-funpath-lab/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 underline"
+              >
+                FunPath Lab
+              </a>
+            </li>
+            <li>
+              <strong>Diogo Couceiro, PhD student</strong>,{" "}
+              <a
+                href="https://sites.google.com/view/ibb-tecnico-bsrg-funpath-lab/home/team/diogo-couceiro"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 underline"
+              >
+                homepage
+              </a>
+            </li>
+            <li>
+              <strong>Inês Costa, PhD student</strong>,{" "}
+              <a
+                href="https://sites.google.com/view/ibb-tecnico-bsrg-funpath-lab/home/team/in%C3%AAs-costa"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 underline"
+              >
+                homepage
+              </a>
+            </li>
+            <li>
+              <strong>Maria Zolotareva, PhD student</strong>,{" "}
+              <a
+                href="https://sites.google.com/view/ibb-tecnico-bsrg-funpath-lab/home/team/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 underline"
+              >
+                homepage
+              </a>
+            </li>
+            <li>
+              <strong>Luís Coutinho, MSc student</strong>,{" "}
+              <a
+                href="https://sites.google.com/view/ibb-tecnico-bsrg-funpath-lab/home/team/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 underline"
+              >
+                homepage
+              </a>
+            </li>
+            <li>
+              <strong>Isabel Sá-Correia, PhD</strong>, Full Professor IST,{" "}
+              <a
+                href="https://ibb.tecnico.ulisboa.pt/people/members/Isabel-Sa-Correia/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 underline"
+              >
+                homepage
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      {/* Cite Section */}
+      <section>
+        <h2 className="text-2xl font-bold mb-4">How to cite Yeastract</h2>
+        <h3 className="text-lg font-bold mb-2">
+          If you use our resources, please cite:
+        </h3>
+        <p className="text-base">
+          M.C. Teixeira, R. Viana, M. Palma, J. Oliveira, M. Galocha, M.N. Mota,
+          D. Couceiro, M.G. Pereira, M. Antunes, I.V. Costa, P. Pais, C. Parada,
+          C. Chaouiya, I. Sá-Correira, P.T. Monteiro (2023),{" "}
+          <b>
+            YEASTRACT+: a portal for the exploitation of global transcription
+            regulation and metabolic model data in yeast biotechnology and
+            pathogenesis
+          </b>
+          , <i>Nucleic Acids Research</i>, 51, D785–D791,{" "}
+          <a
+            href="https://doi.org/10.1093/nar/gkac1041"
+            className="text-blue-500 underline"
+          >
+            doi:10.1093/nar/gkac1041
+          </a>
+        </p>
+      </section>
+    </div>
+  );
 }
--- yet-another-frontend/src/components/Layout.js
+++ yet-another-frontend/src/components/Layout.js
@@ -7,46 +7,40 @@ import SearchIcon from "../svg/SearchIcon.js";
 // import species from "./Species";
 
 export default function Layout() {
-    const [searchFormData, setSearchFormData] = React.useState("");
-    const navigate = useNavigate();
-    const { species } = useParams();
-    // if (speciesList[species] === undefined) return <div>not found</div>; // create 404 page eventually
+  const [searchFormData, setSearchFormData] = React.useState("");
+  const navigate = useNavigate();
+  const { species } = useParams();
+  // if (speciesList[species] === undefined) return <div>not found</div>; // create 404 page eventually
 
-    function handleSearch(event) {
-        event.preventDefault();
-        console.log(searchFormData);
-        if (searchFormData.trim() !== "") {
-            // const res = await searchTerm(searchFormData.trim(), species);
-            // console.log(res);
-            navigate(
-                `/${species}/advanced-search?term=${searchFormData.trim()}`
-            );
-        }
+  function handleSearch(event) {
+    event.preventDefault();
+    console.log(searchFormData);
+    if (searchFormData.trim() !== "") {
+      // const res = await searchTerm(searchFormData.trim(), species);
+      // console.log(res);
+      navigate(`/${species}/advanced-search?term=${searchFormData.trim()}`);
     }
+  }
 
-    function currentPage() {
-        return window.location.pathname.substring(
-            window.location.pathname.lastIndexOf("/") + 1
-        );
-    }
+  function currentPage() {
+    return window.location.pathname.substring(
+      window.location.pathname.lastIndexOf("/") + 1,
+    );
+  }
 
-    return (
-        // <div className="flex flex-row h-full">
-        <div className="drawer lg:drawer-open">
-            <input
-                id="sidebar-toggle"
-                type="checkbox"
-                className="drawer-toggle"
-            />
-            <div className="drawer-content">
-                <div className="flex flex-row gap-2 px-1 items-center">
-                    <label
-                        htmlFor="sidebar-toggle"
-                        aria-label="open sidebar"
-                        className="btn btn-square btn-ghost drawer-button lg:hidden"
-                    >
-                        <OpenSidebarIcon />
-                        {/* <svg
+  return (
+    // <div className="flex flex-row h-full">
+    <div className="drawer lg:drawer-open">
+      <input id="sidebar-toggle" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        <div className="flex flex-row gap-2 px-1 items-center">
+          <label
+            htmlFor="sidebar-toggle"
+            aria-label="open sidebar"
+            className="btn btn-square btn-ghost drawer-button lg:hidden"
+          >
+            <OpenSidebarIcon />
+            {/* <svg
                             className="w-6 h-6 text-gray-800 dark:text-white hidden lg:block"
                             aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg"
@@ -63,132 +57,122 @@ export default function Layout() {
                                 d="m15 19-7-7 7-7"
                             />
                         </svg> */}
-                    </label>
-                    <h1 className="text-2xl font-figtree mt-1 lg:hidden">
-                        YEASTRACT+
-                    </h1>
-                </div>
-                <div className="max-w-[100vw] h-full container mx-auto mt-2">
-                    <Outlet />
-                </div>
-            </div>
-            {/* shadow-2xl shadow-black */}
-            <div className="drawer-side">
-                <label
-                    htmlFor="sidebar-toggle"
-                    aria-label="close sidebar"
-                    className="drawer-overlay"
-                ></label>
-                <div className="flex flex-col p-5 h-screen bg-primary max-w-60">
-                    <label
-                        htmlFor="sidebar-toggle"
-                        aria-label="close sidebar"
-                        className="btn btn-circle btn-ghost self-end lg:hidden"
-                    >
-                        <CloseIcon />
-                    </label>
-                    <a href="/">
-                        <h1 className="hidden lg:block font-figtree text-2xl">
-                            YEASTRACT+
-                        </h1>
-                    </a>
+          </label>
+          <h1 className="text-2xl font-figtree mt-1 lg:hidden">YEASTRACT+</h1>
+        </div>
+        <div className="max-w-[100vw] h-full container mx-auto mt-2">
+          <Outlet />
+        </div>
+      </div>
+      {/* shadow-2xl shadow-black */}
+      <div className="drawer-side">
+        <label
+          htmlFor="sidebar-toggle"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <div className="flex flex-col p-5 h-screen bg-primary max-w-60">
+          <label
+            htmlFor="sidebar-toggle"
+            aria-label="close sidebar"
+            className="btn btn-circle btn-ghost self-end lg:hidden"
+          >
+            <CloseIcon />
+          </label>
+          <a href="/">
+            <h1 className="hidden lg:block font-figtree text-2xl">
+              YEASTRACT+
+            </h1>
+          </a>
 
-                    <select
-                        className="select select-sm select-bordered w-full max-w-xs mb-4 mt-2 text-color"
-                        id="species"
-                        name="species"
-                        value={species}
-                        onChange={(e) => navigate(`/${e.target.value}`)}
-                    >
-                        {Object.keys(speciesList).map((option) => (
-                            <option key={option} value={option}>
-                                {speciesList[option].short}
-                            </option>
-                        ))}
-                    </select>
-                    {/* </label> */}
+          <select
+            className="select select-sm select-bordered w-full max-w-xs mb-4 mt-2 text-color"
+            id="species"
+            name="species"
+            value={species}
+            onChange={(e) => navigate(`/${e.target.value}`)}
+          >
+            {Object.keys(speciesList).map((option) => (
+              <option key={option} value={option}>
+                {speciesList[option].short}
+              </option>
+            ))}
+          </select>
+          {/* </label> */}
 
-                    <form onSubmit={handleSearch}>
-                        {/* <Input
+          <form onSubmit={handleSearch}>
+            {/* <Input
                         isClearable
                         defaultValue={searchFormData}
                         placeholder="Search"
                         onChange={(e) => setSearchFormData(e.target.value)}
                         onClear={() => setSearchFormData("")}
                     /> */}
-                        <label className="input input-sm input-bordered flex items-center gap-2">
-                            <SearchIcon />
-                            <input
-                                type="text"
-                                className="w-full"
-                                placeholder="Search"
-                                id="search"
-                                name="search"
-                                onChange={(e) =>
-                                    setSearchFormData(e.target.value)
-                                }
-                            />
-                        </label>
-                    </form>
-                    <a
-                        href="/advanced-search"
-                        className="link mt-1 mb-6 ml-3 text-sm text-color"
-                    >
-                        Advanced Search
-                    </a>
+            <label className="input input-sm input-bordered flex items-center gap-2">
+              <SearchIcon />
+              <input
+                type="text"
+                className="w-full"
+                placeholder="Search"
+                id="search"
+                name="search"
+                onChange={(e) => setSearchFormData(e.target.value)}
+              />
+            </label>
+          </form>
+          <a
+            href="/advanced-search"
+            className="link mt-1 mb-6 ml-3 text-sm text-color"
+          >
+            Advanced Search
+          </a>
 
-                    <ul className="menu w-full gap-1 text-lg p-0">
-                        <li>
-                            <a
-                                className={`text-color ${
-                                    currentPage() === species
-                                        ? "active-tab"
-                                        : ""
-                                }`}
-                                href={`/${species}`}
-                            >
-                                Regulations
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                className={`text-color ${
-                                    currentPage() === "sequences"
-                                        ? "active-tab"
-                                        : ""
-                                }`}
-                                href={`/${species}/sequences`}
-                            >
-                                Sequences
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                className={`text-color ${
-                                    currentPage() === "about"
-                                        ? "active-tab"
-                                        : ""
-                                }`}
-                                href={`/${species}/about`}
-                            >
-                                About
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                className={`text-color ${
-                                    currentPage() === "help" ? "active-tab" : ""
-                                }`}
-                                href={`/${species}/help`}
-                            >
-                                Help
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+          <ul className="menu w-full gap-1 text-lg p-0">
+            <li>
+              <a
+                className={`text-color ${
+                  currentPage() === species ? "active-tab" : ""
+                }`}
+                href={`/${species}`}
+              >
+                Regulations
+              </a>
+            </li>
+            <li>
+              <a
+                className={`text-color ${
+                  currentPage() === "sequences" ? "active-tab" : ""
+                }`}
+                href={`/${species}/sequences`}
+              >
+                Sequences
+              </a>
+            </li>
+            <li>
+              <a
+                className={`text-color ${
+                  currentPage() === "about" ? "active-tab" : ""
+                }`}
+                href={`/${species}/about`}
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                className={`text-color ${
+                  currentPage() === "help" ? "active-tab" : ""
+                }`}
+                href={`/${species}/help`}
+              >
+                Help
+              </a>
+            </li>
+          </ul>
         </div>
+      </div>
+    </div>
 
-        // </div>
-    );
+    // </div>
+  );
 }
